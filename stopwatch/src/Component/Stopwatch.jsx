c701ad5625b1e9eedd2d52719f39c83a565ac9a4
kuntalkumar <kuntalkumar789@gmail.com>
2023-09-15 22:13:09 +0530

    functionality done

--- stopwatch/src/Component/Stopwatch.jsx
+++ stopwatch/src/Component/Stopwatch.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./Stopwatch.css"
 const Stopwatch = () => {
+
+    const [state,setState]=useState(false);
+    const [reading,setReading]=useState(0)
+
+
+    useEffect(() => {
+      let interval;
+
+
+      if(state===true){
+      interval=setInterval(() => {
+
+        setReading((pre=>pre+1))
+        
+      },1000);
+    }
+    else{
+        clearInterval(interval)
+    }
+
+    return ()=>{
+        clearInterval(interval)
+    }
+    
+    }, [state])
+
+
+
+    const handleStart=()=>{
+        setState(true)
+
+    }
+    
+    const handlePause=()=>{
+            setState(false)
+    }
+    
+    const handleReset=()=>{
+        setState(false)
+        setReading(0)
+    }
   return (
-    <div>
+    <div className='parent'>
       <h1>Stopwatch App</h1>
+        <br />
+      <h2>Timer : {reading}</h2>
+        <br />
+      <button disabled={state===true} onClick={handleStart} className='btn'>Start</button>
+      <button disabled={state===false} onClick={handlePause} className='btn'>Pause</button>
+      <button disabled={reading===0} onClick={handleReset} className='btn'>Reset</button>
 
-      <h2>Timer : 0</h2>
-
-      <button>Start</button>
-      <button>Pause</button>
-      <button>Reset</button>
-      
     </div>
   )
 }
