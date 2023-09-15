479efc4c9f3dd104750363272b0ccd876a6793a8
kuntalkumar <kuntalkumar789@gmail.com>
2023-09-15 20:19:53 +0530

    1st

---
+++ stopwatch/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Stopwatch from './Component/Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+    <Stopwatch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/Component/Stopwatch.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Stopwatch = () => {
+  return (
+    <div>
+      <h1>Stopwatch App</h1>
+
+      <h2>Timer : 0</h2>
+
+      <button>Start</button>
+      <button>Pause</button>
+      <button>Reset</button>
+      
+    </div>
+  )
+}
+
+export default Stopwatch
